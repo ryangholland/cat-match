b2ec821267f6e340e459c1f629e17fb0a7d2d7c6
ryangholland <rghcode@gmail.com>
2024-11-27 11:00:37 -0500

    Add static styling for card grid

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,45 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen ">
       <header className="bg-slate-100">
-        <div className="container mx-auto p-4 max-w-6xl flex">
-          <img src="./src/assets/logo.png" alt="CatMatch logo" className="w-32 h-auto" />
-
+        <div className="container mx-auto p-4 max-w-6xl flex justify-center">
+          <img
+            src="./src/assets/logo.png"
+            alt="CatMatch logo"
+            className="w-32 h-auto"
+          />
         </div>
       </header>
 
-      <main className="flex-grow"></main>
+      <main className="flex-grow flex items-center justify-center">
+        <div className="container mx-auto grid grid-cols-4 gap-2 sm:grid-cols-5 lg:grid-cols-5 md:gap-4 p-4 justify-center place-items-center max-w-2xl">
+     
+
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          <div className="bg-cover bg-center w-24 and h-36 shadow-lg rounded-lg" style={{ backgroundImage: `url(./src/assets/card-back.png)`}}>Card 1</div>
+          
+        </div>
+      </main>
 
       <footer className="bg-slate-100">
         <div className="container mx-auto p-4 text-center">
